f33ad69e4e80ae0765d4bf452cd517452805d352
karthekan-k1 <karthekan441@gmail.com>
2025-05-17 15:25:44 +0530

    day-4

---
+++ sample/script.js
@@ -0,0 +1,94 @@
+/*var a=10
+a="karthe"
+a=30.3
+console.log(a)
+let b=20
+ b="mahi"
+console.log(b)
+const c=30
+console.log(c)
+function hi(){
+    var a=10
+    var b=20
+    console.log(a+b)
+}
+hi()
+function i(a,b){
+    var c=a*b
+    console.log(c)
+}
+i(10,20)
+function fo(){
+    var arr=[]
+    var d=[20,"karthe",1.1,'k']
+    for( var i=0;i<=d.length;i++)
+        console.log(d[i])
+ }
+ fo()
+ var n=50
+ if(n%2==0){
+console.log("even")
+ }
+else{
+console.log("odd")
+}
+for (n=1;n<=10;n++){
+    if(n%2==0){
+        console.log(`${n} is even`)
+    }
+    else{
+          console.log(`${n} is odd`)
+    }
+}
+function check() {
+    var arr = [10, 3, 'cdm laksd', 50]; 
+    var even = [];
+    var odd = [];
+    var strings = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number') {
+            if (arr[i] % 2 === 0) {
+                even.push(arr[i]);
+            } else {
+                odd.push(arr[i]);
+            }
+        } else if (typeof arr[i] === 'string') {
+            strings.push(arr[i]);
+        }
+    }
+
+    console.log("Even numbers:", even);
+    console.log("Odd numbers:", odd);
+    console.log("Strings:", strings);
+}
+
+check();
+n=1;
+while  (n <=10 ){
+    if(n%2==0){
+        console.log(`${n} is even`)
+    }
+    else{
+          console.log(`${n} is odd`)
+    }
+    n++;
+}
+n=1;
+do;
+{
+  if(n%2==0
+
+  ){
+        console.log(`${n} is even`)
+    }
+    else
+    {
+          console.log(`${n} is odd`)
+    }
+    n++;
+}
+while(n<=10);
+n=10
+var res=(n%2===0)?"even":"odd"
+console.log(res)*/
